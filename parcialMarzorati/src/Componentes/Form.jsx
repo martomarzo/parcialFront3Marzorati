3a7aed36a125747c9c3b34757407485218d312a1
martomarzo <martinemarzorati@gmail.com>
2023-05-31 18:44:40 -0300

    primera subida

---
+++ parcialMarzorati/src/Componentes/Form.jsx
@@ -0,0 +1,13 @@
+
+
+function Form() {
+    return (
+    <form>
+        <input type="text" />
+        <input type="text" />
+        <button type="submit">Enviar</button>
+    </form>
+    );
+  }
+  
+  export default Form;
